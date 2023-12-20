9675e81a9d1fd27c5270c4c9036b7b58d19df2fd
yoel-mc <yoel@mulaicoding.id>
2023-12-20 18:38:49 +0700

    gas

--- praktikum_PPT2.py
+++ praktikum_PPT2.py
@@ -1,29 +1,13 @@
-def lengthOfLIS(nums):
-    # Binary search approach
+def lengthOfLIS(nums):    
     n = len(nums)
     ans = []
- 
-    # Initialize the answer list with the
-    # first element of nums
+         
     ans.append(nums[0])
  
     for i in range(1, n):
-        if nums[i] > ans[-1]:
-            # If the current number is greater
-            # than the last element of the answer
-            # list, it means we have found a
-            # longer increasing subsequence.
-            # Hence, we append the current number
-            # to the answer list.
+        if nums[i] > ans[-1]:                                                                        
             ans.append(nums[i])
-        else:
-            # If the current number is not
-            # greater than the last element of
-            # the answer list, we perform
-            # a binary search to find the smallest
-            # element in the answer list that
-            # is greater than or equal to the
-            # current number.
+        else:                                                                                    
             low = 0
             high = len(ans) - 1
             while low < high:
@@ -31,20 +15,11 @@ def lengthOfLIS(nums):
                 if ans[mid] < nums[i]:
                     low = mid + 1
                 else:
-                    high = mid
-            # We update the element at the
-            # found position with the current number.
-            # By doing this, we are maintaining
-            # a sorted order in the answer list.
+                    high = mid                                                
             ans[low] = nums[i]
- 
-    # The length of the answer list
-    # represents the length of the
-    # longest increasing subsequence.
+             
     return len(ans)
  
-# Driver program to test above function
 if __name__ == "__main__":
-    nums = [4, 1, 13, 7, 0, 2, 8, 11, 3]
-    # Function call
+    nums = [4, 1, 13, 7, 0, 2, 8, 11, 3]    
     print("Length of LIS is", lengthOfLIS(nums))
